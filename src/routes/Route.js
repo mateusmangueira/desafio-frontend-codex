cb9a8a908ac5586e057b9f5f42ea089012d29723
mateusmangueira <mateus.mangueira@ccc.ufcg.edu.br>
2021-04-17 15:42:55 -0300

    Primeiro commit, esqueleto do projeto iniciado

---
+++ src/routes/Route.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const signed = false; // Isso quer dizer q toda vez que a aplicacao inicia o usuario ta deslogado
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/sessions" />;
+  }
+
+  if (signed && !isPrivate) {
+    return <Redirect to="/tasks" />;
+  }
+
+  const Layout = signed ? DefaultLayout : AuthLayout; // If ternario para decider qual pagina ira iniar, dependendo do atributo Signed
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+};
+
+RouteWrapper.defaulProps = {
+  isPrivate: false,
+};
---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://backend-desafio-codex.herokuapp.com/', //URL do Heroku com o nosso backend
+});
+
+export default api;
